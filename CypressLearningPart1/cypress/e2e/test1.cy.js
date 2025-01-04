f8f7e9b901b5764e3d5c6b28fa3ebb3ea90c123a
Mugundhan-strugglebreaker <srmvmugund@gmail.com>
2025-01-04 15:45:54 -0800

    remove unwanted test line

--- CypressLearningPart1/cypress/e2e/test1.cy.js
+++ CypressLearningPart1/cypress/e2e/test1.cy.js
@@ -1,5 +1,4 @@
 it('Google Search' , () => {
     cy.visit('https://google.com')
     cy.get('#APjFqb').type('Automation Step By Step{Enter}')
-    cy.get('[data-hveid="CBsQAA"] > .nPDzT > .YmvwI').click()
 })
